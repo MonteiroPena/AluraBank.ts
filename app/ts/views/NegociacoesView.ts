53a190868e25562bdb03b5da31f5d6598d931ddd
MonteiroPena <dmpassis@gmail.com>
2020-06-10 15:19:51 -0300

    Lidando com tipos genéricos

--- app/ts/views/NegociacoesView.ts
+++ app/ts/views/NegociacoesView.ts
@@ -1,11 +1,5 @@
-class NegociacoesView extends View{
+class NegociacoesView extends View<Negociacoes> {
 
-  // pega o resultado do template(), vai atribuir na propriedade innerHTML que vai converter automaticamente em elemento do doom
-  updadte(model: Negociacoes): void {
-    this._elemento.innerHTML = this.template(model);
-  }
-
-  // define o que eu quero exibir para o usuario
   template(model: Negociacoes): string {
     return `
     <table class="table table-hover table-bordered">
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,7 +1,16 @@
-class View {
+class View<T> {
   protected _elemento: Element;
 
   constructor(seletor: string) {
     this._elemento = document.querySelector(seletor);
+  }
+
+  // pega o resultado do template(), vai atribuir na propriedade innerHTML que vai converter automaticamente em elemento do doom
+  update(model: T) {
+    this._elemento.innerHTML = this.template(model);
+  }
+  // define o que eu quero exibir para o usuario
+  template(model: T): string {
+    throw new Error("Você deve implementar o método template");
   }
 }
