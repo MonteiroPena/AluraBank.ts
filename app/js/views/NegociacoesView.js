b9be2b7781b2571187a91f9951d15b8a1361c270
MonteiroPena <dmpassis@gmail.com>
2020-06-10 14:49:02 -0300

    Template dinâmico

--- app/js/views/NegociacoesView.js
+++ app/js/views/NegociacoesView.js
@@ -3,11 +3,11 @@ class NegociacoesView {
         this._elemento = document.querySelector(seletor);
     }
     // pega o resultado do template(), vai atribuir na propriedade innerHTML que vai converter automaticamente em elemento do doom
-    updadte() {
-        this._elemento.innerHTML = this.template();
+    updadte(model) {
+        this._elemento.innerHTML = this.template(model);
     }
     // define o que eu quero exibir para o usuario
-    template() {
+    template(model) {
         return `
     <table class="table table-hover table-bordered">
       <thead>
@@ -20,6 +20,23 @@ class NegociacoesView {
       </thead>
 
       <tbody>
+        ${model
+            .paraArray()
+            .map((negociacao) => {
+            return `
+            <tr>
+              <td>
+                ${negociacao.data.getDate()} /
+                ${negociacao.data.getMonth() + 1} /
+                ${negociacao.data.getFullYear()}
+              </td>
+              <td>${negociacao.quantidade}</td>
+              <td>${negociacao.valor}</td>
+              <td>${negociacao.volume}</td>
+            </tr>
+          `;
+        })
+            .join('')}
       </tbody>
 
       <tfoot>
--- app/ts/views/NegociacoesView.ts
+++ app/ts/views/NegociacoesView.ts
@@ -6,12 +6,12 @@ class NegociacoesView {
   }
 
   // pega o resultado do template(), vai atribuir na propriedade innerHTML que vai converter automaticamente em elemento do doom
-  updadte(): void {
-    this._elemento.innerHTML = this.template();
+  updadte(model: Negociacoes): void {
+    this._elemento.innerHTML = this.template(model);
   }
 
   // define o que eu quero exibir para o usuario
-  template(): string {
+  template(model: Negociacoes): string {
     return `
     <table class="table table-hover table-bordered">
       <thead>
@@ -24,6 +24,23 @@ class NegociacoesView {
       </thead>
 
       <tbody>
+        ${model
+          .paraArray()
+          .map((negociacao) => {
+            return `
+            <tr>
+              <td>
+                ${negociacao.data.getDate()} /
+                ${negociacao.data.getMonth() + 1} /
+                ${negociacao.data.getFullYear()}
+              </td>
+              <td>${negociacao.quantidade}</td>
+              <td>${negociacao.valor}</td>
+              <td>${negociacao.volume}</td>
+            </tr>
+          `;
+          })
+          .join('')}
       </tbody>
 
       <tfoot>
