6510fb3e1d8e0143a146024863bd62e3fda7b36e
MonteiroPena <dmpassis@gmail.com>
2020-06-11 11:06:08 -0300

    Lapidando o codigo

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,14 +1,11 @@
-import { NegociacoesView } from "../views/NegociacoesView";
-import { MensagemView } from "../views/MensagemView";
-import { Negociacoes } from "../models/Negociacoes";
-import { Negociacao } from "../models/Negociacao";
-
+import { NegociacoesView, MensagemView } from "../views/index";
+import { Negociacoes, Negociacao } from "../models/index";
 export class NegociacaoController {
   private _inputData: JQuery;
   private _inputQuantidade: JQuery;
   private _inputValor: JQuery;
   private _negociacoes = new Negociacoes();
-  private _negociacoesView = new NegociacoesView("#negociacoesView");
+  private _negociacoesView = new NegociacoesView("#negociacoesView", true); // true "ativa" o escapar do View.ts = que desativa todas as tags scripts da string que vai pro template
   private _mensagemView = new MensagemView("#mensagemView");
 
   constructor() {
@@ -21,8 +18,17 @@ export class NegociacaoController {
   adiciona(event: Event) {
     event.preventDefault();
 
+    let data = new Date(this._inputData.val().replace(/-/g, ","));
+
+    if (!this._ehDiaUtil(data)) {
+      this._mensagemView.update(
+        "Somente negociações emm dias úteis, por favor"
+      );
+      return;
+    }
+
     const negociacao = new Negociacao(
-      new Date(this._inputData.val().replace(/-/g, ",")),
+      data,
       parseInt(this._inputQuantidade.val()),
       parseFloat(this._inputValor.val())
     );
@@ -31,4 +37,22 @@ export class NegociacaoController {
     this._negociacoesView.update(this._negociacoes);
     this._mensagemView.update("Negociação adicionada com sucesso!");
   }
+
+  private _ehDiaUtil(data: Date) {
+    return (
+      data.getDay() != DiaDaSemana.Sabado &&
+      data.getDay() != DiaDaSemana.Domingo
+    );
+  }
+}
+
+// 0 = domingo e 6 = sabado
+enum DiaDaSemana {
+  Domingo,
+  Segunda,
+  Terca,
+  Quarta,
+  Quinta,
+  Sexta,
+  Sabado,
 }
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,14 +1,23 @@
 /* Quando vc tem uma classe que tem "um burraco" que alguem precisa implementar e que essa classe não faz sentido alguem criar-la diretamente, vc pode usar o modificador ABSTRACT = ela nao deixa vc criar uma instancia */
 export abstract class View<T> {
   protected _elemento: JQuery;
-
-  constructor(seletor: string) {
+  private _escapar: boolean;
+  // "?" indica que o parãmetro é opcional, e quando é opcinal ele vai como undefined
+  constructor(seletor: string, escapar: boolean = false) {
     this._elemento = $(seletor);
+    this._escapar = escapar;
   }
 
   /* Pega o resultado do template(), vai atribuir na propriedade innerHTML que vai converter automaticamente em elemento do doom */
   update(model: T) {
-    this._elemento.html(this.template(model));
+    let template = this.template(model);
+
+    /* "escapar" a tag <script> / essa expressao regular procura a incidencia da tag scrip em qualquer lugar da string, é trocada por uma string em branco */
+    if (this._escapar) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/g, "");
+    }
+
+    this._elemento.html(template);
   }
   /* Define o que eu quero exibir para o usuario */
   abstract template(model: T): string;
